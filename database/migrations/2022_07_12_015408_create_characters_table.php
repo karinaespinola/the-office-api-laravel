ae6d86f98f13f1248f6e6b49c16318c419a02ede
Karina Espinola <k.espinolah@gmail.com>
2022-07-12 15:40:57 -0600

    Change to the characters table

--- database/migrations/2022_07_12_015408_create_characters_table.php
+++ database/migrations/2022_07_12_015408_create_characters_table.php
@@ -15,7 +15,8 @@ return new class extends Migration
     {
         Schema::create('characters', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->timestamps();
         });
     }
--- database/seeders/CharacterSeeder.php
+++ database/seeders/CharacterSeeder.php
@@ -16,40 +16,52 @@ class CharacterSeeder extends Seeder
     public function run()
     {
       DB::table('characters')->insert([
-        'name' => 'Michael Scott',
+        'first_name' => 'Michael',
+        'last_name'  => 'Scott',
       ]);
       DB::table('characters')->insert([
-        'name' => 'Dwight Schrute',
+        'first_name' => 'Dwight',
+        'last_name'  => 'Schrute',
       ]);
       DB::table('characters')->insert([
-        'name' => 'Pam Halpert',
+        'first_name' => 'Pam',
+        'last_name'  => 'Halpert',
       ]);
       DB::table('characters')->insert([
-        'name' => 'Jim Halpert',
+        'first_name' => 'Jim',
+        'last_name'  => 'Halpert',
       ]);
       DB::table('characters')->insert([
-        'name' => 'Andrew Bernard',
+        'first_name' => 'Andrew',
+        'last_name'  => 'Bernard',
       ]);
       DB::table('characters')->insert([
-        'name' => 'Phyllis Vance',
+        'first_name' => 'Phyllis',
+        'last_name'  => 'Vance',
       ]);
       DB::table('characters')->insert([
-        'name' => 'Stanley Hudson',
+        'first_name' => 'Stanley',
+        'last_name'  => 'Hudson',
       ]);
       DB::table('characters')->insert([
-        'name' => 'Oscar Martinez',
+        'first_name' => 'Oscar',
+        'last_name'  => 'Martinez',
       ]);
       DB::table('characters')->insert([
-        'name' => 'Angela Martin',
+        'first_name' => 'Angela',
+        'last_name'  => 'Martin',
       ]);
       DB::table('characters')->insert([
-        'name' => 'Kevin Malone',
+        'first_name' => 'Kevin',
+        'last_name'  => 'Malone',
       ]);
       DB::table('characters')->insert([
-        'name' => 'Ryan Howard',
+        'first_name' => 'Ryan',
+        'last_name'  => 'Howard',
       ]);
       DB::table('characters')->insert([
-        'name' => 'Kelly Kapoor',
+        'first_name' => 'Kelly',
+        'last_name'  => 'Kapoor',
       ]);
     }
 }
